e5015d92d6b871c3f13617bb383d5f06dafc0227
Jake Brukhman <jbrukh@gmail.com>
2011-04-22 23:01:41 -0400

    Added bitvector unit test.

---
+++ bitvector_test.go
@@ -0,0 +1,150 @@
+package bitvector
+
+import "testing"
+
+// Length of the BitVector
+// for the benchmarking portion
+const VECTOR_LEN = 1024
+
+
+func assert( t *testing.T, value bool, format string, args ...interface{} ) {
+    if !value {
+		t.Errorf(format,args...)
+    }
+}
+
+func TestAllocationSize(t *testing.T) {
+	// test single byte allocation
+    for i := 1; i <= 8; i++ {	
+		v := New(uint(i))
+		assert(t, len(v.bits)==1, "wrong allocation size for iteration %d", i)
+	}
+    // test double byte allocation
+    for i := 9; i <= 16; i++ {	
+		v := New(uint(i))
+		assert(t, len(v.bits)==2, "wrong allocation size for iteration %d", i)
+	}
+
+    // test array growth
+    v := New(1)
+    v.Set(1,true)
+    assert(t,len(v.bits)==1, "weird allocation after set")
+    v.Set(7,true)
+    assert(t,len(v.bits)==1, "weird allocation after set")
+    v.Set(8,true)
+    assert(t,len(v.bits)==2, "weird allocation after set (should have grown to 16)")
+    w := New(1)
+    w.Set(17,true)
+    assert(t,len(w.bits)==3, "didn't grow correctly (should have grown to 3 bytes)")
+}
+
+func TestSetGet(t *testing.T) {
+	v := New(8)
+    w := New(16)
+    v.Set(0,true)
+    assert(t,v.Get(0),"didn't set correctly")
+    assert(t,v.GetInt(0)==1,"didn't set correctly")
+    for i := 1; i < 8; i++ {
+		assert(t,!v.Get(uint(i)),"should be 0")
+		assert(t,v.GetInt(uint(i))==0,"GetInt should be 0")
+	}
+
+    // set all bits in w
+    for i := 0; i < 16; i++ {
+        w.Set(uint(i),true)
+    }
+    // and check...
+    for i := 0; i < 16; i++ {
+        assert(t,w.Get(uint(i)),"should be 1")
+		assert(t,w.GetInt(uint(i))==1,"GetInt should be 1")
+    }
+}
+
+func TestNot(t *testing.T) {
+	w := New(16)
+	w.Not()
+    // and check...
+    for i := 0; i < 16; i++ {
+        assert(t,w.Get(uint(i)),"should be 1")
+		assert(t,w.GetInt(uint(i))==1,"GetInt should be 1")
+    }
+	w.Not()
+    // and check...
+    for i := 0; i < 16; i++ {
+        assert(t,!w.Get(uint(i)),"should be 0")
+		assert(t,w.GetInt(uint(i))==0,"GetInt should be 0")
+    }
+	w.Not()
+    // and check...
+    for i := 0; i < 16; i++ {
+        assert(t,w.Get(uint(i)),"should be 1")
+		assert(t,w.GetInt(uint(i))==1,"GetInt should be 1")
+    }
+}
+
+func TestNotPackage(t *testing.T) {
+    v := New(16)
+    w := Not(v)
+    assert(t,v!=w,"v and w are the same")
+    // and check...
+    for i := 0; i < 16; i++ {
+        assert(t,w.Get(uint(i)),"should be 1")
+		assert(t,w.GetInt(uint(i))==1,"GetInt should be 1")
+    }
+    z := Not(w)
+    // and check...
+    for i := 0; i < 16; i++ {
+        assert(t,!z.Get(uint(i)),"should be 0")
+		assert(t,z.GetInt(uint(i))==0,"GetInt should be 0")
+    }
+}
+
+func TestCopy(t *testing.T) {
+	v := New(16)
+	w := v.Copy()
+	assert(t,v!=w,"same object?")
+	assert(t,len(v.bits)==len(w.bits),"different byte allocations!")
+
+}
+
+func TestEqual(t *testing.T) {
+	v := New(16)
+	w := New(8)
+	assert(t,v.Equal(w),"%s should be equal to %s", v, w)
+}
+
+func BenchmarkAllocation(b *testing.B) {
+    for i := 0; i < b.N; i++ {
+		New(VECTOR_LEN)
+    }
+}
+
+func BenchmarkNot(b *testing.B) {
+	v := New(VECTOR_LEN)
+	for i := 0; i < b.N; i++ {
+		v.Not()
+	}
+}
+
+func BenchmarkNotManual(b *testing.B) {
+	v := New(VECTOR_LEN)
+    for i := 0; i < b.N; i++ {
+		for j := 0; j < VECTOR_LEN; j++ {
+			v.Set(uint(j),true)
+		}
+    }
+}
+
+func BenchmarkSetTrue(b *testing.B) {
+	v := New(VECTOR_LEN)
+	for i := 0; i < b.N; i++ {
+		v.Set(0,true)
+	}
+}
+
+func BenchmarkSetFalse(b *testing.B) {
+	v := New(VECTOR_LEN)
+	for i := 0; i < b.N; i++ {
+		v.Set(0,false)
+	}
+}
