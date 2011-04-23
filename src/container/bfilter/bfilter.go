98043a013ffc84cf7ac45cd663b8f6e78237261d
Jake Brukhman <jbrukh@gmail.com>
2011-04-22 20:51:05 -0400

    Save.

--- src/container/bfilter/bfilter.go
+++ src/container/bfilter/bfilter.go
@@ -1,7 +1,23 @@
 package bfilter
 
+import (
+    . "container/bitvector"
+    "crypto/md5"
+    "encoding/binary"
+    //"fmt"
+)
+
 
 type BloomFilter struct {
-    length int
-    poop int
+    v *BitVector
 }
+
+func Hash( obj interface{} ) []byte {
+    digest := md5.New()
+    if err := binary.Write(digest, binary.LittleEndian, obj); err != nil {
+        panic(err)
+    }
+    result := digest.Sum()
+    return result
+}
+
